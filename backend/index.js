6cbcc0426776f392a2d63d0aa58940da6f1292eb
idominikbrame <idominikbrame@gmail.com>
2022-02-04 03:47:59 -0600

    added registration in backend, updated searchbar functionality

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,26 @@ app.get('/', (req, res) => {
 
 app.post(`/aws`, (req, res) => {
     pool.query(
-        "SELECT * FROM users where email = ? AND pass = ?", [req.body.email, req.body.password], (error, rows) => {
+        "SELECT * FROM users where email = ? AND pass = ?",
+        [req.body.email, req.body.password], (error, rows) => {
             console.log( rows)
             if(rows[0]) {
                 res.json({"success": "access granted"})
             } else {
                 res.json({"failure": "no access"})
             }
+        })
+})
+
+app.post('/register', (req, res) => {
+    console.log(req.body)
+    pool.query(
+        "INSERT INTO users (email, first_name, last_name, pass) VALUES (?, ? , ?, ?)",
+        [req.body.email, req.body.first_name, req.body.last_name, req.body.password, 0], (error, result) => {
+            if(error) {
+                return console.log('error:' + error)
+            }
+            console.log("result:" + result[0])
         })
 })
 app.listen(port, () => {
